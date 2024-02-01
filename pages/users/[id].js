997ca1dfc01e2228ca70b840d6e5eae6c15fe53f
Vladimir988 <optimist0706@gmail.com>
2024-02-01 13:20:04 +0200

    gitignore cache updated

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,7 +1,10 @@
+import {useRouter} from "next/router";
+
 export default function () {
+  const {query} = useRouter()
     return(
       <div>
-        User
+        User with id: {query.id}
       </div>
     );
 };
